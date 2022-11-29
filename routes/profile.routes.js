d6003f2ce927191622621132e641aa832a588147
Ricardo Isidro <ricardo.isidro33@gmail.com>
2022-11-29 14:15:34 +0000

    some routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,35 +1,69 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User.model')
-
+const User = require("../models/User.model");
 
 //Edit profile
 
-router.put('/profile/:id', async(req,res,next)=>{
-    const {id} = req.params;
-    const { firstName, lastName} = req.body
-    try {
-       
-            const updatedProfile = await User.findByIdAndUpdate(id, {firstName, lastName},{new: true})
-            res.status(200).json(updatedProfile)
-          
-    } catch (error) {
-        next(error)
-}                                                                                                                                                                                                   
-                                
-})
-
+router.put("/profile/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const {
+    firstName,
+    lastName,
+    email,
+    genre,
+    birthdayDate,
+    phoneNumber,
+    residenceArea,
+    formationArea,
+    formationDegree,
+    interestAreas,
+    interests,
+    description,
+  } = req.body;
+  try {
+    const updatedProfile = await User.findByIdAndUpdate(
+      id,
+      {
+        firstName,
+        lastName,
+        email,
+        genre,
+        birthdayDate,
+        phoneNumber,
+        residenceArea,
+        formationArea,
+        formationDegree,
+        interestAreas,
+        interests,
+        description,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProfile);
+  } catch (error) {
+    next(error);
+  }
+});
 
 //Delete profile
-router.delete('/profile/:id',async(req,res,next)=>{
-    const {id} = req.params
-try {
-    await User.findByIdAndDelete(id)
-    res.status(200).json({message: `The  with Id ${id} was deleted`})
+router.delete("/profile/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ message: `The  with Id ${id} was deleted` });
+  } catch (error) {
+    next(error);
+  }
+});
 
-} catch (error) {
-    next(error)
-}
-})
+router.get("/profile/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const foundUser = await User.findById(id);
+    res.status(200).json(foundUser);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports= router;
+module.exports = router;
